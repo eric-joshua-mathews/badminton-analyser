3e1870a45d197e6c27bbecc489eee4c314917f80
eric-joshua-mathews <74616931+eric-joshua-mathews@users.noreply.github.com>
2026-02-18 11:45:30 +0000

    merged videoless event handlers
    POA:
    add markers
    add coloured bg to indicate which player hits
    add some form of Player at the bottom shuttle at the top
    store player 1 shots every next button press
    return guess shot evalutation
    FLIP everything for player 2

--- static/JS/videoless.js
+++ static/JS/videoless.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     //court and wrapper
 
 const court = document.getElementById("court");
-const wrapper = document.getElementById("court-wrapper"); //maybe not needed
 const state = {
     player:1,
     playerPos:null,
@@ -26,66 +25,40 @@ const courtZones = {
 } //maybe useless
 
 
-court.addEventListener("click",handleLeftClick); //Player
-court.addEventListener("contextmenu",handleRightClick); //Shuttle
+court.addEventListener("click",handleClick);
+court.addEventListener("contextmenu",handleClick);
 
 function getClickContext (e){
     //const target=e.target;
     const group = e.target.closest("g")
     const rect = e.target.closest("rect")
-    if (!group || !court.contains(group))
+    if (!group || !court.contains(group)||!rect)
         return null;
 
     return {group : group , zone: rect.id};
 }
 
-
-function handleRightClick(e) { //shuttle
-    e.preventDefault()
-    const {group, zone} = getClickContext(e)
-    if(!group)
-        return null;
-    state.shuttlePos = group.dataset.zone; //parent -> shuttle or player side
-    const out = isOutOfBounds(state.shuttlePos);
-    //const marker = placeMarker(x,y,"shuttle");
-    if (out){
-        M.toast({html: 'Please be aware the shuttle is out.',classes: 'red darken-2'});
-    }
-
-}
 function isOutOfBounds(type){
       return type!=="Shuttle";
 }
-
-function handleLeftClick(e) { //player
-    e.preventDefault()
-    const {group, zone} = getClickContext(e)
-    if(!group)
-        return null;
-    state.playerPos = group.dataset.zone;
-    const invalidPlayer = isNotValidPlayer(state.playerPos);
-
-//for debugging
-    console.log(type, "- group")
-    console.log(state.playerPos, "- zone")
-
-
-    //place marker
-    if (invalidPlayer){
-        M.toast({html: 'Please be aware the player cannot be there.',classes: 'red darken-2'});
-    }
-}
 function isNotValidPlayer(type){
         return type==="Shuttle"||type===undefined ;
-
-
- function handleClick(e){
-     e.preventDefault()
-     const {group , zone} = getClickContext(e)
+}
+function getSVGCoords(e){
+const pt = court.createSVGPoint();
+pt.x = e.clientX;
+pt.y = e.clientY;
+return pt.matrixTransform(court.getScreenCTM().inverse())
+}
+function handleClick(e){
+     e.preventDefault();
+     ctx= getClickContext(e)
+     if (!ctx) return;
+     const {group , zone} = ctx;
      if (!group){
          return null
      }
-     if (e.type==="click"){ //right
+     if (e.type==="click"){ //left click
          state.playerPos = group.dataset.zone; //CHECK TO SEE IF IT NEEDA BE TYPE
          const invalidPlayer = isNotValidPlayer(state.playerPos);
          //add marker
@@ -93,13 +66,11 @@ function isNotValidPlayer(type){
             M.toast({html: 'Please be aware the player cannot be there.',classes: 'red darken-2'});
          }
      }
-     if (e.type ==="contextmenu"){
+     if (e.type ==="contextmenu"){ // right click
          state.shuttlePos=group.dataset.zone //CHECK TO SEE IF IT NEEDA BE TYPE
          if (isOutOfBounds(state.shuttlePos)){
             M.toast({html: 'Please be aware the shuttle is out.',classes: 'red darken-2'});
          }
      }
-
  }
-}
 });
